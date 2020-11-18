ebd6a782e50338b126e21c61d83352767e1180e5
BARRY <aliou.barry@novasdux-solutions.com>
2020-11-18 11:33:27 +0100

    Partie Web de Bankhi

---
+++ src/main/java/com/immo/Bankhi/ManageImmoGuineeWebApplication.java
@@ -0,0 +1,13 @@
+package com.immo.Bankhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageImmoGuineeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageImmoGuineeWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/immo/Bankhi/ManageImmoGuineeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immo.Bankhi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageImmoGuineeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
